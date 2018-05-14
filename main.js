772a0728388fafb799f68f7c53179da5f02f35e1
Dawid Buchwald <dawid.buchwald@hotmail.com>
2018-05-14 22:00:08 +0200

    Added new function with a test
    Please note - tests don't pass just yet

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ var getGreeting = function() {
   return 'Hello World!';
 };
 
+var getNamedGreeting = function() {
+  return '';
+}
+
 module.exports.getGreeting = getGreeting;
+module.exports.getNamedGreeting = getNamedGreeting;
--- main.test.js
+++ main.test.js
@@ -5,3 +5,9 @@ test('getGreeting function should return valid greeting', () => {
 
   expect(greeting).toBe('Hello World!');
 });
+
+test('getNamedGreeting function should return valid greeting', () => {
+  const greeting = main.getNamedGreeting('Dawid');
+
+  expect(greeting).toBe('Hello, Dawid!');
+});
