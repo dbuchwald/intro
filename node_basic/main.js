446c052d6b6b8181614058a7a41172867edf9932
Dawid Buchwald <dawid.buchwald@hotmail.com>
2018-05-19 13:45:24 +0200

    Moved basic project to node_basic folder

--- node_basic/main.js
+++ node_basic/main.js
@@ -6,5 +6,8 @@ const getNamedGreeting = function(name) {
   return 'Hello, ' + name + '!';
 }
 
+console.log('Standard greeting (no parameters): ' + getGreeting());
+console.log('Parameterized greeting with parameter "Node": ' + getNamedGreeting('Node'));
+
 module.exports.getGreeting = getGreeting;
 module.exports.getNamedGreeting = getNamedGreeting;
