78f619c792faa6783bafcea64e176b108e79a458
Dawid Buchwald <dawid.buchwald@hotmail.com>
2018-05-20 14:11:29 +0200

    Initial implementation of sample ES6 project using babel-node

---
+++ babel_basic/src/main.test.js
@@ -0,0 +1,13 @@
+import { getGreeting, getNamedGreeting } from "./main";
+
+test('getGreeting function should return valid greeting', () => {
+  const greeting = getGreeting();
+
+  expect(greeting).toBe('Hello World!');
+});
+
+test('getNamedGreeting function should return valid greeting', () => {
+  const greeting = getNamedGreeting('Dawid');
+
+  expect(greeting).toBe('Hello, Dawid!');
+});
