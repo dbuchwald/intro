56b2ebae23c03c75b8e3641bae52bdd4ea315bcb
Dawid Buchwald <dawid.buchwald@hotmail.com>
2018-05-28 20:48:32 +0200

    Initial implementation of Babel Mini program (debug not working yet)

---
+++ babel_mini/main.js
@@ -0,0 +1,12 @@
+export const getGreeting = () => {
+  return 'Hello World!';
+};
+
+export const getNamedGreeting = (name) => {
+  return 'Hello, ' + name + '!';
+};
+
+console.log('Standard greeting (no parameters): ' + getGreeting());
+console.log('Parameterized greeting with parameter "Node": ' + getNamedGreeting('Node'));
+
+
